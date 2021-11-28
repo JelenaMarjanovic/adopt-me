5f2f31ab1e2c73f257922b31c43b8e68c442f544
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-11-28 01:21:54 +0100

    Implement-Render the Pet component

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ const Pet = () => {
 };
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!')
-  );
+  return React.createElement('div', {}, [
+    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet)
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
