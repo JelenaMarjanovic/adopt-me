867363817cf37d0e24ac9c563701dad441c9f9ea
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-12-19 01:59:21 +0100

    Formatted code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 const Pet = ({ name, animal, bread }) => {
-  return React.createElement('div', {}, [
-    React.createElement('h2', {}, name),
-    React.createElement('h3', {}, animal),
-    React.createElement('h3', {}, bread)
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h3", {}, animal),
+    React.createElement("h3", {}, bread),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', { id: 'my-brand' }, 'Adopt Me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Bora',
-      animal: 'Dog',
-      bread: 'Poodle'
+      name: "Bora",
+      animal: "Dog",
+      bread: "Poodle",
     }),
     React.createElement(Pet, {
-      name: 'Cici',
-      animal: 'Cat',
-      bread: 'Domestic Cat'
+      name: "Cici",
+      animal: "Cat",
+      bread: "Domestic Cat",
     }),
     React.createElement(Pet, {
-      name: 'Zak',
-      animal: 'Parrot',
-      bread: 'Budgerigar'
-    })
+      name: "Zak",
+      animal: "Parrot",
+      bread: "Budgerigar",
+    }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
